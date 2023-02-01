6ca50504125cf2d58de0320d08077ae5cbcdfc6a
Maria Paz Abarca <mp.abarca.alvear@gmail.com>
2023-02-01 11:46:24 +0100

    Add fontawesome icon to project and navbar

--- src/components/desktopNavbar.jsx
+++ src/components/desktopNavbar.jsx
@@ -2,6 +2,9 @@ import Link from 'next/link';
 import { navLinks } from '../data/navbarLinks';
 import Image from 'next/image';
 import logo from '../../public/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const DesktopNavbar = () => {
     return (
@@ -23,6 +26,20 @@ const DesktopNavbar = () => {
                         </Link>
                 )
                 })}
+            </ul>
+            <ul className='menu-rrss'>
+                <Link href='https://github.com/mpabarca'>
+                    <FontAwesomeIcon icon={faGithub} />
+                </Link>
+                <Link href='https://www.linkedin.com/in/mpabarca/'>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </Link>
+                <Link href='https://www.instagram.com/pacita_tua/'>
+                    <FontAwesomeIcon icon={faInstagram} />
+                </Link>
+                <Link href='mailto:mp.abarca.alvear@gmail.com'>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                </Link>
             </ul>
         </nav>
     )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,10 @@ import '@/styles/index.scss'
 import Head from 'next/head'
 import DesktopNavbar from '../components/desktopNavbar';
 import MobileNavbar from '../components/mobileNavbar';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,16 +16,18 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DesktopNavbar />
-      <MobileNavbar />
-      <section className='content'>
-        <Component {...pageProps} />
-        <footer>
-          <a href="https://www.flaticon.com/free-icons/moon" title="moon icons" rel="noreferrer" target="_blank">
-            Moon icons created by Freepik - Flaticon
-          </a>
-        </footer>
-      </section>
+      <div className='main'>
+        <DesktopNavbar />
+        <MobileNavbar />
+        <div className='content'>
+          <Component {...pageProps} />
+          <footer>
+            <a href="https://www.flaticon.com/free-icons/moon" title="moon icons" rel="noreferrer" target="_blank">
+              Moon icons created by Freepik - Flaticon
+            </a>
+          </footer>
+        </div>
+      </div>
     </>
   )
 }
